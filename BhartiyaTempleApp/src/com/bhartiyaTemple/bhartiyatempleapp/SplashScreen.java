54c0dd4d9ea95043748bf5337b2ffafb5ccc1718
kdevarajan001 <venkylee@gmail.com>
2013-09-21 18:06:56 -0400

    initial splash screen

---
+++ BhartiyaTempleApp/src/com/bhartiyaTemple/bhartiyatempleapp/SplashScreen.java
@@ -0,0 +1,65 @@
+package com.bhartiyaTemple.bhartiyatempleapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+
+public class SplashScreen extends Activity {
+
+	private static final int SPLASH_DISPLAY_TIME = 4000; // splash screen delay
+															// time
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash_screen);
+		 Thread splashThread = new Thread() {
+		 @Override
+		 public void run() {
+		 try {
+		 int waited = 0;
+		 while (waited < 3000) {
+		 sleep(20);
+		 waited += 40;
+		 }
+		 } catch (InterruptedException e) {
+		 // do nothing
+		 } finally {
+		 finish();
+		 Intent i = new Intent();
+		 i.setClassName("com.bhartiyaTemple.bhartiyatempleapp",
+		 "com.bhartiyaTemple.bhartiyatempleapp.MainActivity");
+		 startActivity(i);
+		 }
+		 }
+		 };
+		 splashThread.start();
+		
+
+//		new Handler().postDelayed(new Runnable() {
+//			public void run() {
+//
+//				Intent intent = new Intent();
+//				intent.setClass(SplashScreen.this, MainActivity.class);
+//
+//				SplashScreen.this.startActivity(intent);
+//				SplashScreen.this.finish();
+//
+//				// transition from splash to main menu
+//				overridePendingTransition(R.animate.activityfadein,
+//						R.animate.splashfadeout);
+//
+//			}
+//		}, SPLASH_DISPLAY_TIME);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.splash_screen, menu);
+		return true;
+	}
+
+}
